1bf7f33ef92996034721535cc7840f89424bcf6f
twanakhudhur <twana.ahmed.5595@gmail.com>
2024-08-30 14:56:10 +0300

    Add Basic NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Navbar from "./components/navbar/Navbar";
 import "./styles/layout.scss";
 
 function App() {
-  return <div className="layout">Brno Immobilien</div>;
+  return <div className="layout">
+    <Navbar />
+  </div>;
 }
 
 export default App;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import "./navbar.scss";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <div className="left">
+        <a href="/" className="logo">
+          <img src="/logo.png" alt="" />
+          <span>BrnoImmobilien</span>
+        </a>
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+        <a href="/">Agents</a>
+      </div>
+      <div className="right">
+        <a href="/">Sign in</a>
+        <a href="/" className="register">
+          Sign up
+        </a>
+      </div>
+    </nav>
+  );
+}
