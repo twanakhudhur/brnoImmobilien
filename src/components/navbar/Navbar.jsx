cfb0a91e7669664eec70bf68951b6d04340aaedc
twanakhudhur <twana.ahmed.5595@gmail.com>
2024-08-30 15:37:10 +0300

    Responsive the navbar, Add Toggle Button

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import "./navbar.scss";
 
 export default function Navbar() {
+  const [open, setOpen] = useState(false);
   return (
     <nav>
       <div className="left">
@@ -18,6 +20,22 @@ export default function Navbar() {
         <a href="/" className="register">
           Sign up
         </a>
+        <div
+          className={`menuIcon ${open ? "open" : ""}`}
+          onClick={() => setOpen((prev) => !prev)}
+        >
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+        <div className={open ? "menu active" : "menu"}>
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agents</a>
+          <a href="/">Sign in</a>
+          <a href="/">Sign up</a>
+        </div>
       </div>
     </nav>
   );
