d42c9bd55190035cc481eef4e9aba74809e0a0f5
twanakhudhur <twana.ahmed.5595@gmail.com>
2024-08-31 14:16:20 +0300

    Add Map to List Page

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,5 +1,62 @@
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
 import "./map.scss";
+import "leaflet/dist/leaflet.css";
+import Pin from "../pin/Pin";
 
-export default function Map() {
-  return <div>Map</div>;
+function Map({ items }) {
+  const initialPosition = [52.4797, -1.90269];
+  const initialZoom = 7;
+
+  return (
+    <MapContainer
+      center={initialPosition}
+      zoom={initialZoom}
+      scrollWheelZoom={true}
+      className="map"
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {items.map((item) => (
+        <Pin item={item} key={item.id} />
+      ))}
+      <ResetViewButton
+        initialPosition={initialPosition}
+        initialZoom={initialZoom}
+      />
+    </MapContainer>
+  );
 }
+
+export default Map;
+
+const ResetViewButton = ({ initialPosition, initialZoom }) => {
+  const map = useMap();
+
+  const handleResetView = () => {
+    map.setView(initialPosition, initialZoom);
+  };
+
+  return (
+    <button
+      onClick={handleResetView}
+      className="reset"
+      style={{
+        position: "absolute",
+        backgroundColor: "rgba(255, 255, 255, 0.8)",
+        padding: "5px",
+        cursor: "pointer",
+        width: "25px",
+        height: "25px",
+        borderRadius: "50%",
+        top: "10px",
+        right: "10px",
+        border: "none",
+        zIndex: 1000,
+      }}
+    >
+      <img src="/refresh.png" width={"100%"} alt="refresh" />
+    </button>
+  );
+};
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,25 +1,26 @@
 import { useState } from "react";
 import "./navbar.scss";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   const [open, setOpen] = useState(false);
   return (
     <nav>
       <div className="left">
-        <a href="/" className="logo">
+        <Link href="/" className="logo">
           <img src="/logo.png" alt="" />
           <span>BrnoImmobilien</span>
-        </a>
-        <a href="/">Home</a>
-        <a href="/">About</a>
-        <a href="/">Contact</a>
-        <a href="/">Agents</a>
+        </Link>
+        <Link href="/">Home</Link>
+        <Link href="/">About</Link>
+        <Link href="/">Contact</Link>
+        <Link href="/">Agents</Link>
       </div>
       <div className="right">
-        <a href="/">Sign in</a>
-        <a href="/" className="register">
+        <Link href="/">Sign in</Link>
+        <Link href="/" className="register">
           Sign up
-        </a>
+        </Link>
         <div
           className={`menuIcon ${open ? "open" : ""}`}
           onClick={() => setOpen((prev) => !prev)}
@@ -29,12 +30,12 @@ export default function Navbar() {
           <div className="bar"></div>
         </div>
         <div className={open ? "menu active" : "menu"}>
-          <a href="/">Home</a>
-          <a href="/">About</a>
-          <a href="/">Contact</a>
-          <a href="/">Agents</a>
-          <a href="/">Sign in</a>
-          <a href="/">Sign up</a>
+          <Link href="/">Home</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
+          <Link href="/">Agents</Link>
+          <Link href="/">Sign in</Link>
+          <Link href="/">Sign up</Link>
         </div>
       </div>
     </nav>
