e1a11cf9235cbfcaed6e968f0f87ecd0859456f2
twanakhudhur <twana.ahmed.5595@gmail.com>
2024-08-31 15:53:04 +0300

    Add Slider

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "./src/styles/_variables.scss";`,
+        additionalData: `@import "./src/styles/_variables.scss";
+        @import "./src/styles/responsive.scss";`,
       },
     },
   },
