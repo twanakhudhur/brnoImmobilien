1213b07cf3c4100416275f52818f006bf9ef7764
twanakhudhur <twana.ahmed.5595@gmail.com>
2024-08-31 16:15:39 +0300

    Add Chat box and window

--- src/routes/profilePage/ProfilePage.jsx
+++ src/routes/profilePage/ProfilePage.jsx
@@ -1,3 +1,4 @@
+import Chat from "../../components/chat/Chat";
 import List from "../../components/list/List";
 import "./profilePage.scss";
 
@@ -37,7 +38,9 @@ function ProfilePage() {
         </div>
       </div>
       <div className="chatContainer">
-        <div className="wrapper">Chat</div>
+        <div className="wrapper">
+          <Chat />
+        </div>
       </div>
     </div>
   );
