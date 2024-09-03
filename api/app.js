a4424f4c263fe19484471e3896a8eb13d10f4d4e
twanakhudhur <twana.ahmed.5595@gmail.com>
2024-09-03 17:03:26 +0300

    Add Post Routes And Controllers

--- api/app.js
+++ api/app.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 
 import authRoute from "./routes/auth.route.js";
 import userRoute from "./routes/user.route.js";
+import postRoute from "./routes/post.route.js";
 
 dotenv.config();
 
@@ -19,6 +20,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
 
 app.listen(8800, () => {
   console.log("Server is Running...");
--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,12 +1,13 @@
-import express from 'express';
+import express from "express";
+import {verifyToken} from "../middleware/verifyToken.js";
+import { addPost, deletePost, getPost, getPosts, updatePost } from "../controllers/post.controller.js";
 
-const router =  express.Router();
+const router = express.Router();
 
-
-router.get('/test',(req, res)=>{
-    res.send("Hello from Router")
-} )
-
-
+router.get("/", getPosts);
+router.get("/:id", getPost);
+router.post("/", verifyToken, addPost);
+router.put("/:id", verifyToken, updatePost);
+router.delete("/:id", verifyToken, deletePost);
 
 export default router;
