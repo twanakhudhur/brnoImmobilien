5b4036035d01ba9bcce1678153310cec179b8b8f
twanakhudhur <twana.ahmed.5595@gmail.com>
2024-09-08 22:17:09 +0300

    Update vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,16 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
   css: {
     preprocessorOptions: {
       scss: {
